2402c74249320fff74cbcb902f4cdcad2b3884b6
atilol <ggrkssk@gmail.com>
2020-05-25 15:14:31 +0900

    update

--- src/codeLayers.ts
+++ src/codeLayers.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as Diff from 'diff';
 
 export class LayerProvider implements vscode.TreeDataProvider<LayerItem> {
 
@@ -114,4 +115,110 @@ export class LayerItem extends vscode.TreeItem {
 	};
 
 	contextValue = 'codelayer';
+}
+
+// let decorator: vscode.TextEditorDecorationType;
+class DecLines {
+	ranges: vscode.Range[] = [];
+	decorator: vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({});
+}
+let decLines: DecLines;
+
+export function colorDiff() {
+	if (decLines !== undefined) {
+		decLines.decorator.dispose();
+	}
+
+	decLines = {
+		ranges: [],
+		decorator: vscode.window.createTextEditorDecorationType({
+			'isWholeLine': true,
+			'borderWidth': '1px',
+			'borderRadius': '2px',
+			'borderStyle': 'solid',
+			'light': {
+				'backgroundColor': 'rgba(58, 70, 101, 0.3)',
+				'borderColor': 'rgba(58, 70, 101, 0.4)',
+			},
+			'dark': {
+				'backgroundColor': 'rgba(117, 141, 203, 0.3)',
+				'borderColor': 'rgba(117, 141, 203, 0.4)',
+			}
+		})
+	};
+
+	let curFilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+	if (curFilePath === undefined) {
+		console.log("Failed to get a fsPath.");
+		return;
+	}
+	const curDir = path.dirname(curFilePath);
+	const layerDir = path.join(curDir, ".layer");
+
+	const baseLayerPath = path.join(layerDir, "base.txt");
+	const newLayerPath = path.join(layerDir, "new.txt");
+
+	fs.copyFileSync(curFilePath, newLayerPath);
+
+	// TODO: set a character encoding of a target file
+	const baseLayer = fs.readFileSync(baseLayerPath, "utf-8");
+	const curFile = fs.readFileSync(curFilePath, "utf-8");
+
+	const diff = Diff.diffLines(baseLayer, curFile);
+	console.log(diff);
+	let startLine = 0;
+	diff.forEach(part => {
+		if (part.count === undefined) {
+			return;
+		}
+		if (part.removed) {
+			return;
+		}
+		if (!part.added) {
+			startLine += part.count;
+			return;
+		}
+		const endLine = startLine + part.count - 1;
+		const range = new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine, 0));
+		decLines.ranges.push(range);
+
+		startLine = endLine + 1;
+	});
+
+	const editor = vscode.window.activeTextEditor;
+	if (editor === undefined) { return; }
+
+	editor.setDecorations(decLines.decorator, decLines.ranges);
+}
+
+export function hideNewLayer() {
+	let curFilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+	if (curFilePath === undefined) {
+		console.log("Failed to get a fsPath.");
+		return;
+	}
+	const curDir = path.dirname(curFilePath);
+	const layerDir = path.join(curDir, ".layer");
+
+	const baseLayerPath = path.join(layerDir, "base.txt");
+
+	fs.copyFileSync(baseLayerPath, curFilePath);
+
+	if (decLines !== undefined) {
+		decLines.decorator.dispose();
+	}
+}
+
+export function exposeNewLayer() {
+	let curFilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+	if (curFilePath === undefined) {
+		console.log("Failed to get a fsPath.");
+		return;
+	}
+	const curDir = path.dirname(curFilePath);
+	const layerDir = path.join(curDir, ".layer");
+	const newLayerPath = path.join(layerDir, "new.txt");
+	const newLayer = fs.readFileSync(newLayerPath, "utf-8");
+	fs.writeFileSync(curFilePath, newLayer);
+	setTimeout(colorDiff, 100);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 
 import * as vscode from 'vscode';
 
-import { LayerProvider, LayerItem } from './codeLayers';
+import { LayerProvider, LayerItem, colorDiff, hideNewLayer, exposeNewLayer } from './codeLayers';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,13 +10,27 @@ export function activate(context: vscode.ExtensionContext) {
 	const codeLayersProvider = new LayerProvider();
 	vscode.window.registerTreeDataProvider('codeLayers', codeLayersProvider);
 	vscode.commands.registerCommand('codeLayers.refreshEntry', () => codeLayersProvider.refresh());
-	vscode.commands.registerCommand('extension.selectLayer', moduleName => vscode.window.showInformationMessage(`${moduleName} is selected.`));
+
+	vscode.commands.registerCommand('extension.selectLayer', moduleName => {
+		hideNewLayer();
+		vscode.window.showInformationMessage(`${moduleName} is selected.`);
+	});
 
 	vscode.commands.registerCommand('codeLayers.addEntry', () => {
 		codeLayersProvider.addNewLayer();
 	});
 
 
-	vscode.commands.registerCommand('codeLayers.editEntry', (node: LayerItem) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+	vscode.commands.registerCommand('codeLayers.editEntry', (node: LayerItem) => {
+		exposeNewLayer();
+		vscode.window.showInformationMessage(`enable layer ${node.label}.`);
+	});
+
+
 	vscode.commands.registerCommand('codeLayers.deleteEntry', (node: LayerItem) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+
+	// And when modifying the document
+	vscode.workspace.onDidSaveTextDocument(() => {
+		colorDiff();
+	}, null, context.subscriptions);
 }
