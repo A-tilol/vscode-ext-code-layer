4c629d5606b8c1d1bf7c2cbdbc0a193ee95af421
atilol <ggrkssk@gmail.com>
2020-05-28 09:25:35 +0900

    add commands to subscriptions

--- src/extension.ts
+++ src/extension.ts
@@ -5,29 +5,31 @@ import * as vscode from 'vscode';
 import { LayerProvider, LayerItem, Utils } from './codeLayers';
 
 export function activate(context: vscode.ExtensionContext) {
-
+	let disposable: vscode.Disposable;
 	const codeLayersProvider = new LayerProvider();
 	vscode.window.registerTreeDataProvider('codeLayers', codeLayersProvider);
 
-	vscode.commands.registerCommand('codeLayers.addEntry', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('codeLayers.addEntry', () => {
 		codeLayersProvider.addLayer();
-	});
+	}));
 
-	vscode.commands.registerCommand('codeLayers.mergeEntry', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('codeLayers.mergeEntry', () => {
 		codeLayersProvider.mergeLayer();
-	});
+	}));
 
-	vscode.commands.registerCommand('codeLayers.deleteEntry', (layer: LayerItem) => {
+	context.subscriptions.push(vscode.commands.registerCommand('codeLayers.deleteEntry', () => {
 		codeLayersProvider.deleteLayer();
-	});
+	}));
 
 	// when a layer item on a view is selected
-	vscode.commands.registerCommand('extension.selectLayer', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.selectLayer', () => {
 		codeLayersProvider.toggleLayerVisibility();
-	});
+	}));
 
 	// refresh a layer tree view
-	vscode.commands.registerCommand('codeLayers.refreshEntry', () => codeLayersProvider.refresh());
+	context.subscriptions.push(vscode.commands.registerCommand('codeLayers.refreshEntry', () => {
+		codeLayersProvider.refresh();
+	}));
 
 	vscode.workspace.onDidSaveTextDocument(() => {
 		if (codeLayersProvider.getLayers()[0].isVisible) {
